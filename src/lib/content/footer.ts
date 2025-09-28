9ad21e7135194f120e01e73f043af4723fa7c62a
Shrishti2004 <shrishtibandooni@gmail.com>
2025-09-28 21:27:43 +0530

    update github link

--- src/lib/content/footer.ts
+++ src/lib/content/footer.ts
@@ -2,5 +2,5 @@ import { FooterSectionType } from '@/lib/types/sections';
 
 export const footerSection: FooterSectionType = {
   title: 'Design & Built by Shrishti Bandooni',
-  link: 'https://shrishti-portfolio-seven.vercel.app/home',
+  link: 'https://github.com/Shrishti2004/ShrishtiPortfolio.git',
 };
